4ed0c864b33f02aec597f5d16efbf7b3cbd3e5c0
kondlekaryash <yashk9100@gmail.com>
2025-03-15 14:42:52 +1100

    role/login-based middleware checks added

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -2,20 +2,19 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  console.log(authHeader);
   const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({
       success: false,
-      message: "Access denied. No token provided. Please login to continue",
+      message:
+        "Access denied. No Auth-token provided. Please login to continue",
     });
   }
 
   //decode this token
   try {
     const decodedTokenInfo = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    console.log(decodedTokenInfo);
 
     req.userInfo = decodedTokenInfo;
     next();
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/home", homeRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is now listeining to PORT ${PORT}`);
